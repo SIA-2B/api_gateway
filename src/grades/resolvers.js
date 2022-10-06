6b7b1006fc83ce3c33a6b865727a8666ad5d88df
mvalbuenag <39029585+mvalbuenag@users.noreply.github.com>
2022-10-06 00:16:01 -0500

    Actualizacion de API (grades)

--- src/grades/resolvers.js
+++ src/grades/resolvers.js
@@ -1,22 +1,33 @@
 import { generalRequest, getRequest } from '../utilities';
 import { url, port, entryPoint } from './server';
 
-const URL = `http://${url}:${port}/${entryPoint}`;
+//const URL = `http://${url}:${port}/${entryPoint}`;
+const URL = `http://${url}/${entryPoint}`;
 
 const resolvers = {
 	Query: {
 		allGrades: (_) =>
-			getRequest(URL, ''),
+			generalRequest(`${URL}/grades`, 'GET'),
 		gradeById: (_, { id }) =>
-			generalRequest(`${URL}/${id}`, 'GET'),
+			generalRequest(`${URL}/grades/${id}`, 'GET'),
+		allSchedules: (_) =>
+			generalRequest(`${URL}/schedules`, 'GET'),
+		scheduleById: (_, { id }) =>
+			generalRequest(`${URL}/schedules/${id}`, 'GET')
 	},
 	Mutation: {
 		createGrade: (_, { grade }) =>
-			generalRequest(`${URL}/`, 'POST', grade),
+			generalRequest(`${URL}/grades`, 'POST', grade),
 		updateGrade: (_, { id, grade }) =>
-			generalRequest(`${URL}/${id}`, 'PUT', grade),
+			generalRequest(`${URL}/grades/${id}`, 'PUT', grade),
 		deleteGrade: (_, { id }) =>
-			generalRequest(`${URL}/${id}`, 'DELETE')
+			generalRequest(`${URL}/grades/${id}`, 'DELETE'),
+		createSchedule: (_, { grade }) =>
+			generalRequest(`${URL}/schedules`, 'POST', grade),
+		updateSchedule: (_, { id, grade }) =>
+			generalRequest(`${URL}/schedules/${id}`, 'PUT', grade),
+		deleteSchedule: (_, { id }) =>
+			generalRequest(`${URL}/schedules/${id}`, 'DELETE')
 	}
 };
 
