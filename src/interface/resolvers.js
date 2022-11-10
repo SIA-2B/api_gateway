9a7a9e18f08ee7bb03d59de95477090e2a404171
cdmorau23 <cdmorau@gmail.com>
2022-11-10 01:29:46 -0500

    Interface with ngrok

--- src/interface/resolvers.js
+++ src/interface/resolvers.js
@@ -2,10 +2,10 @@ import { generalRequest, getRequest } from '../utilities';
 import { url, port, entryPoint } from './server';
 import personalInfoResolvers from '../personalInfo/resolvers';
 
-const URL = `http://${url}:${port}/${entryPoint}`;
+//const URL = `http://${url}:${port}/${entryPoint}`;
 //import {finalcialInfo} from '../Micro';
 
-// const URL = `http://${url}/${entryPoint}`;
+const URL = `http://${url}/${entryPoint}`;
 //const URL = `http://${finalcialInfo}/${entryPoint}`;
 
 const resolvers = {
