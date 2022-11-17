09f447a281cc92afc0977f7618450a36aa3f55f6
ndcontrerasr <ndcontrerasr@unal.edu.co>
2022-11-17 10:17:32 -0500

    courses

--- src/courses/server.js
+++ src/courses/server.js
@@ -1,3 +1,3 @@
-export const url = '172.20.0.3' // 'host.docker.internal'
+export const url = '34.138.146.137' // 'host.docker.internal'
 export const port = '4001'
 export const entryPoint = 'api/search'
