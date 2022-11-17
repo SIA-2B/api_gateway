361dca9d6dc2b381de8883b821395c761dedd59c
ndcontrerasr <ndcontrerasr@unal.edu.co>
2022-11-16 22:47:45 -0500

    implementacion en google cloud

--- src/personalInfo/server.js
+++ src/personalInfo/server.js
@@ -1,3 +1,4 @@
-export const url = '172.20.0.5'
+// export const url = '172.20.0.5'
+export const url = '104.198.150.53'
 export const port = '3000'
 export const entryPoint = 'api' //NO COLOCAR / A NINGUNO
