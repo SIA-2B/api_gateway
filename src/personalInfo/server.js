d661b9ee2c94bce44940512a5711dd34d1945a72
ndcontrerasr <ndcontrerasr@unal.edu.co>
2022-11-17 09:38:04 -0500

    1

--- src/personalInfo/server.js
+++ src/personalInfo/server.js
@@ -1,4 +1,4 @@
 // export const url = '172.20.0.5'
-export const url = '34.23.86.163'
+export const url = '104.196.223.83'
 export const port = '3000'
 export const entryPoint = 'api' //NO COLOCAR / A NINGUNO
