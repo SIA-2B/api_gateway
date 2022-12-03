5b2274d1afc4be6c8b7e8e38d69a0882faa033c0
ndcontrerasr <ndcontrerasr@unal.edu.co>
2022-12-03 12:44:55 -0500

    interface

--- src/interface/server.js
+++ src/interface/server.js
@@ -1,4 +1,4 @@
 //export const url = 'host.docker.internal' //windows host.docker.internal
-export const url = 'd6bf-200-118-61-100.ngrok.io'
+export const url = '10.116.5.111'
 export const port = '3004'
 export const entryPoint = 'api'
